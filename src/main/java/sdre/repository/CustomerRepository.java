e1158e5b77fde58a6ecd35356c226387134c33ab
jcmellado <jcmellado@gmail.com>
2017-07-04 00:15:39 +0200

    Added custom named repository with a private method.

--- src/main/java/sdre/repository/CustomerRepository.java
+++ src/main/java/sdre/repository/CustomerRepository.java
@@ -1,8 +1,17 @@
 package sdre.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import sdre.domain.Customer;
 
+@RepositoryRestResource(path = "people")
 public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+
+  @RestResource(exported = false)
+  List<Customer> findByName(@Param("name") String name);
 }
